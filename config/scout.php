1b76fb30164e31340b54c7f59e6b51f220943485
Crescenzo De Martino <cresckamina@gmail.com>
2020-09-29 18:39:21 +0200

    Search filter bar v 0.1

--- config/scout.php
+++ config/scout.php
@@ -99,8 +99,8 @@ return [
     */
 
     'algolia' => [
-        'id' => env('ALGOLIA_APP_ID', 'R5T8D67G08'),
-        'secret' => env('ALGOLIA_SECRET', 'db54a81fe7c55de368f97d6db021c1af'),
+        'id' => env('ALGOLIA_APP_ID', 'E2Y25HUG5Z'),
+        'secret' => env('ALGOLIA_SECRET', '615c41075437b39d7ae80d7fd63644dd'),
     ],
 
 ];
--- resources/views/partials/search-partials/filters.blade.php
+++ resources/views/partials/search-partials/filters.blade.php
@@ -1,4 +1,10 @@
 {{-- filtra in base ai km --}}
 <div class="form-group">
-  <input name="rad" type="number" class="form-control" id="form-rad"  placeholder="Raggio in km" />
+  <input name="rad" type="number" class="form-control" id="form-rad"  placeholder="Raggio in km" value="{{request()->input('rad')}}" />
+</div>
+
+<div class="">
+  <input name="minRooms" type="number" placeholder="Numero camere" value="{{request()->input('minRooms')}}">
+  <input name="minBeds" type="number" placeholder="Numero letti" value="{{request()->input('minBeds')}}">
+  <input name="minBaths" type="number" placeholder="Numero cessi" value="{{request()->input('minBaths')}}">
 </div>
