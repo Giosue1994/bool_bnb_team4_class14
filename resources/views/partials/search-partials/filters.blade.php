f077c2d258d43ede90ad33f05141ccad4b13d775
Giosue1994 <giosuelentini1994@outlook.com>
2020-09-29 16:29:05 +0200

    Sistemazione struttura cartelle e file

---
+++ resources/views/partials/search-partials/filters.blade.php
@@ -0,0 +1,4 @@
+{{-- filtra in base ai km --}}
+<div class="form-group">
+  <input name="rad" type="number" class="form-control" id="form-rad"  placeholder="Raggio in km" />
+</div>
---
+++ resources/views/partials/search-partials/search-city_address.blade.php
@@ -0,0 +1,15 @@
+{{-- struttura per la ricerca in base alle città e gli indirizzi --}}
+<div class="form-group">
+  <label for="form-city">Cerca appartamenti</label>
+  <input id="form-city" name="city" type="text" class="form-control" placeholder="Città/Via" />
+</div>
+
+{{-- latitudine e longitudine rimangono display: none perchè non sappiamo ancora nasconderli per bene --}}
+<div class="split" style="display: none">
+  <div class="form-group">
+    <input name="lat" type="text" class="form-control" id="form-lat" placeholder="Latitude" />
+  </div>
+  <div class="form-group">
+    <input name="lng" type="text" class="form-control" id="form-lng" placeholder="Longitude" />
+  </div>
+</div>
