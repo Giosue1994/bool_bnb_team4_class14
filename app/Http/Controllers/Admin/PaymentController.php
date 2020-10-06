fcf3f2ec016009e29bc6fd4bcb7a5797febe3acd
Giosue1994 <giosuelentini1994@outlook.com>
2020-10-06 13:38:41 +0200

    Sponsor
    è possibile acquistare più sponsor per un appartamento

--- app/Http/Controllers/Admin/PaymentController.php
+++ app/Http/Controllers/Admin/PaymentController.php
@@ -9,6 +9,7 @@ use Braintree;
 use App\Apartment;
 use App\User;
 use App\Sponsor;
+use Carbon\Carbon;
 
 class PaymentController extends Controller
 {
@@ -47,14 +48,13 @@ class PaymentController extends Controller
 
     $this_id = $apartment->id;
 
-    $thisSponsor = $apartment->sponsors()->wherePivot('apartment_id', $apartment->id)->get();
-    // dd($thisSponsor);
+    // $thisSponsor = $apartment->sponsors()->wherePivot('apartment_id', $apartment->id)->get();
+    $price = implode($data['sponsors']);
 
-    $amount = $request->amount;
     $nonce = $request->payment_method_nonce;
 
     $result = $gateway->transaction()->sale([
-        'amount' => $amount,
+        'amount' => $price,
         'paymentMethodNonce' => $nonce,
         // 'customer' => [
         //     'firstName' => 'Tony',
@@ -70,8 +70,37 @@ class PaymentController extends Controller
         $transaction = $result->transaction;
         // header("Location: transaction.php?id=" . $transaction->id);
 
-        // $qualcosa = $apartment->sponsors()->attach($data['sponsors']);
-        // dd($qualcosa);
+        if ($price == 2.99) {
+          $duration = 1;
+          $sponsor_id = 1;
+        }elseif ($price == 5.99) {
+          $duration = 3;
+          $sponsor_id = 2;
+        }else {
+          $duration = 6;
+          $sponsor_id = 3;
+        }
+
+        $activeSponsor = $apartment->sponsors()->wherePivot('apartment_id', $apartment->id)->get();
+
+        if ($activeSponsor->isEmpty()) {
+          $startSponsor = Carbon::now()->format('Y-m-d H:i:s');
+        }else {
+          $startSponsor = $apartment->sponsors()
+            ->wherePivot('apartment_id', $apartment->id)
+            ->orderBy('fine_sponsorizzazione', 'desc')
+            ->first()
+            ->getOriginal('pivot_fine_sponsorizzazione');
+          // dd($sponsorRows);
+        }
+
+        $apartment->sponsors()->attach($apartment->id,
+          [
+            'inizio_sponsorizzazione' => $startSponsor,
+            'fine_sponsorizzazione' => Carbon::parse($startSponsor)->addDay($duration),
+            'sponsor_id' => $sponsor_id,
+            'status_payment' => 'approvato',
+          ]);
 
         return back()->with('success_message', 'Transaction successful. The ID is:'. $transaction->id);
     } else {
